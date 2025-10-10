be9ac206824e2774b1dd83a5c31954a931f9d724
dmenini <davide.menini96@gmail.com>
2025-10-10 15:40:39 +0200

    feat: add main modules

---
+++ agent/settings.py
@@ -0,0 +1,9 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    aws_default_region: str = "eu-central-1"
+    aws_access_key_id: str = ""
+    aws_secret_access_key: SecretStr = ""
+    aws_session_token: SecretStr = ""
