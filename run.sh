61de110e5e6ca91c1d32b6bd3a629502d81ec1d2
taamedag <Davide.Menini@swisscom.com>
2025-10-10 15:32:19 +0200

    feat: setup project

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ ! "$AWS_DEFAULT_REGION" ]; then
+  export AWS_DEFAULT_REGION=eu-central-1
+  echo "No AWS_DEFAULT_REGION set, will use default: $AWS_DEFAULT_REGION"
+fi
+
+if [[ -z "$AWS_ACCESS_KEY_ID" ]]; then
+  echo 'Please define AWS_ACCESS_KEY_ID in your environment variables.'
+  exit 1
+fi
+
+if [[ -z "$AWS_SECRET_ACCESS_KEY" ]]; then
+  echo 'Please define AWS_SECRET_ACCESS_KEY in your environment variables.'
+  exit 1
+fi
+
+if [[ -z "$AWS_SESSION_TOKEN" ]]; then
+  echo 'Please define AWS_SESSION_TOKEN in your environment variables.'
+  exit 1
+fi
+
+PYTHONPATH="." streamlit run agent/app.py
