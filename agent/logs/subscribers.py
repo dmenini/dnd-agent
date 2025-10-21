0dccb0d7827d5ef6877d6607b1365d5886e2f42b
dmenini <davide.menini96@gmail.com>
2025-10-21 18:21:11 +0200

    fix: logs

--- agent/logs/subscribers.py
+++ agent/logs/subscribers.py
@@ -9,10 +9,11 @@ console = Console()
 
 def rich_printer(event: Event) -> None:
     verbosity = int(os.getenv("VERBOSITY", Verbosity.DETAIL))
+    always_log = {LogLevel.MAIN, LogLevel.MAP, LogLevel.HEADER}
 
     if (
         (event.type == LogLevel.DEBUG and verbosity >= Verbosity.DEBUG)
         or (event.type == LogLevel.DETAIL and verbosity >= Verbosity.DETAIL)
-        or (event.type == LogLevel.MAIN and verbosity >= Verbosity.MAIN)
+        or (event.type in always_log)
     ):
         console.print(event)
