29dc5b55efbee1660a806530fee1ca8fdfddcc98
dmenini <davide.menini96@gmail.com>
2025-10-21 11:49:16 +0200

    fix: event logging with verbosity

--- agent/logs/subscribers.py
+++ agent/logs/subscribers.py
@@ -1,9 +1,18 @@
+import os
+
 from rich.console import Console
 
-from agent.logs.events import Event
+from agent.logs.events import Event, LogLevel, Verbosity
 
 console = Console()
 
 
 def rich_printer(event: Event) -> None:
-    console.print(event)
+    verbosity = int(os.getenv("VERBOSITY", Verbosity.DETAIL))
+
+    if (
+        (event.type == LogLevel.DEBUG and verbosity >= Verbosity.DEBUG)
+        or (event.type == LogLevel.DETAIL and verbosity >= Verbosity.DETAIL)
+        or (event.type == LogLevel.MAIN and verbosity >= Verbosity.MAIN)
+    ):
+        console.print(event)
