c1ce263d9cb570dc72573e25d84b604ecf4872de
dmenini <davide.menini96@gmail.com>
2025-10-30 09:45:19 +0100

    feat: setup theme, header and footer

--- agent/ui/game_ui.py
+++ agent/ui/game_ui.py
@@ -7,7 +7,7 @@ from textual.app import App, ComposeResult
 from textual.command import CommandInput
 from textual.containers import Horizontal, Vertical
 from textual.driver import Driver
-from textual.widgets import Input
+from textual.widgets import Footer, Header, Input
 
 from agent.ai.graph import build_graph
 from agent.models.config import Config
@@ -31,62 +31,34 @@ class GameUI(App):
         ansi_color: bool = False,
     ) -> None:
         self._state = initial_state
-        self._log_panel: LogPanel | None = None
-        self._map_panel: MapPanel | None = None
-        self._char_panel: CharacterPanel | None = None
-        self._command_input: Input | None = None
         self.input_queue: Queue = asyncio.Queue()
         super().__init__(driver_class, css_path, watch_css, ansi_color)
 
         self.graph = build_graph(config=config.agent)
-
-    @property
-    def log_panel(self) -> LogPanel:
-        if self._log_panel is None:
-            raise ValueError
-        return self._log_panel
-
-    @property
-    def map_panel(self) -> MapPanel:
-        if self._map_panel is None:
-            raise ValueError
-        return self._map_panel
-
-    @property
-    def char_panel(self) -> CharacterPanel:
-        if self._char_panel is None:
-            raise ValueError
-        return self._char_panel
-
-    @property
-    def command_input(self) -> Input:
-        if self._command_input is None:
-            raise ValueError
-        return self._command_input
+        self.title = "DnD Agent"
 
     def compose(self) -> ComposeResult:
+        yield Header()
         with Vertical():
             with Horizontal():
-                with Vertical(classes="left"):
-                    self._map_panel = MapPanel(classes="map")
-                    self._log_panel = LogPanel(classes="logs")
-                    yield self._map_panel
-                    yield self._log_panel
+                with Vertical():
+                    yield MapPanel(id="map", classes="map")
+                    yield LogPanel(id="logs", classes="logs")
 
-                self._char_panel = CharacterPanel(classes="character")
-                yield self.char_panel
+                yield CharacterPanel(id="character", classes="character")
 
-            self._command_input = CommandInput(classes="inp", placeholder="Press ENTER to start game...")
-            yield self.command_input
+            yield CommandInput(id="user-input", classes="inp", placeholder="Press ENTER to start game...")
+        yield Footer()
 
     def on_mount(self) -> None:
         self.update_state(self._state)
+        self.theme = "monokai"
 
     def update_state(self, state: State) -> None:
         """Render a new state."""
-        self.map_panel.update_state(state)
-        self.log_panel.update_state(state)
-        self.char_panel.update_state(state)
+        self.query_one("#map", MapPanel).update_state(state)
+        self.query_one("#logs", LogPanel).update_state(state)
+        self.query_one("#character", CharacterPanel).update_state(state)
 
     async def on_input_submitted(self, event: Input.Submitted) -> None:
         command = event.value.strip()
@@ -101,8 +73,9 @@ class GameUI(App):
 
     async def advance_graph(self) -> None:
         """Manually step the graph."""
-        self.command_input.placeholder = "Thinking..."
-        self.command_input.refresh()
+        command_input = self.query_one("#user-input", CommandInput)
+        command_input.placeholder = "Thinking..."
+        command_input.refresh()
 
         state = await self.graph.ainvoke(
             self._state,
@@ -116,5 +89,5 @@ class GameUI(App):
             if self._state.is_player_turn
             else "Enemy's turn, press ENTER to continue"
         )
-        self.command_input.placeholder = placeholder
-        self.command_input.refresh()
+        command_input.placeholder = placeholder
+        command_input.refresh()
